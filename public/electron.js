008ae0f79de58ea565807f091d9ee3efd555cb6c
Francesco Esposito <francesco.esposito@biotecnomed.it>
2021-03-15 14:48:15 +0100

    time zero

---
+++ public/electron.js
@@ -0,0 +1,41 @@
+const electron = require("electron");
+
+const app = electron.app;
+
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
+
+function createWindow() {
+
+    mainWindow = new BrowserWindow({ fullscreen : 'true'});
+
+    mainWindow.removeMenu();
+
+    mainWindow.loadURL("http://localhost:3000");
+
+    mainWindow.on("closed", () => (mainWindow = null));
+
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+
+    if (process.platform !== "darwin") {
+
+        app.quit();
+
+    }
+
+});
+
+app.on("activate", () => {
+
+    if (mainWindow === null) {
+
+        createWindow();
+
+    }
+
+});
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Applicazione Fullscreen </h1>
+      <h1>Tempo zero</h1>
+    </div>
+  );
+}
+
+export default App;
